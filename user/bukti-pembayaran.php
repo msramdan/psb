50f39414c869cd42f20357341155a65407b0523b
Rafael Nuansa <iandrewnew@gmail.com>
2022-06-30 11:28:24 +0700

    Create Upload Bukti Pembayaran Page

--- user/bukti-pembayaran.php
+++ user/bukti-pembayaran.php
@@ -17,10 +17,10 @@ $id_pendaftaran = $_SESSION['id'];
 $email = $_SESSION['email'];
 $get_pendaftar = mysqli_query($conn, "SELECT * FROM tb_pendaftaran WHERE id_pendaftaran='$id_pendaftaran'");
 $row = mysqli_fetch_array($get_pendaftar);
-
-
+$verifkasi_email = $row['verifikasi_email'];
 $get_pembayaran = mysqli_query($conn, "SELECT * FROM tb_pembayaran WHERE id_pendaftaran='$id_pendaftaran'");
 $bayar = mysqli_fetch_array($get_pembayaran);
+
 ?>
 
 <!DOCTYPE html>
@@ -30,6 +30,10 @@ $bayar = mysqli_fetch_array($get_pembayaran);
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pendaftaran TPA Assyifa</title>
+    <!-- My Swalert -->
+    <link rel="../stylesheet" href="swalert/sweetalert2.min.css"> 
+    <script src="../swalert/sweetalert2.min.js"></script>
+    <script src="../swalert/sweetalert2.all.min.js"></script>
     <!-- My Icon -->
     <link rel="shortcut icon"  href="../img/Logo Assyifa2021.png" />
     <!-- My Bootstrap Icon -->
@@ -51,7 +55,7 @@ $bayar = mysqli_fetch_array($get_pembayaran);
             <p>Mohon cek dan verfikasi email : <?php echo $row['email'];?></p>
             <?php endif;?>
                <!-- Mencetak bukti pendaftaran dari id pendaftaran yang dipanggil dan membukanya pada tab browser baru serta bisa di save ke PDF -->
-               <a href="cetak-bukti.php?id=<?php echo $row['id_pendaftaran'] ?>" target="_blank" class="btn btn-success mt-3 rounded-pill"><i class="bi bi-printer-fill"></i> Cetak Bukti Daftar</a>
+            <a href="cetak-bukti.php?id=<?php echo $row['id_pendaftaran'] ?>" target="_blank" class="btn btn-success mt-3 rounded-pill"><i class="bi bi-printer-fill"></i> Cetak Bukti Daftar</a>
             <a class="btn btn-primary mt-3 rounded-pill" href="bukti-pembayaran.php"><i class="bi bi-cloud-arrow-up-fill"></i> Upload Bukti Pembayaran</a>
             </div>
             <div class="col-md-8">
@@ -61,8 +65,8 @@ $bayar = mysqli_fetch_array($get_pembayaran);
                 </div>
                 <div class="col-md-8 p-3">
                     <div class="card-body">
-                        <h5 class="card-title">Terima Kasih telah mendaftar</h5>
-                        <p class="card-text">Pendaftaran Anda telah <span class="badge rounded-pill bg-success">Sedang direview oleh Admin</span>.</p>
+                        <h5 class="card-title">Upload Bukti Pembayaran</h5>
+                        <p class="card-text">Kode  <span class="badge rounded-pill bg-success">Sedang direview oleh Admin</span>.</p>
                         <p class="card-text">Agar mempermudah proses review, mohon konfirmasi email</p>
                         <p class="card-text">Kehadiran Anda sangat berarti untuk meningkatkan Visi & Misi kami dalam membangun karakter yang lebih baik.</p>
                         <p class="card-text"><small class="text-muted"><?= "Waktu: " . date("Y-m-d h:i:sa"); ?></small></p>
@@ -74,7 +78,20 @@ $bayar = mysqli_fetch_array($get_pembayaran);
     </section>
 
     <?php include('../_partials/user/footer.php');?>
-
+<?php 
+if ($row['verifikasi_email'] == 0):?>
+<script>
+    setTimeout(function() {
+        Swal.fire({
+            title: 'Oops...',
+            text: 'Mohon Verifikasi Email terlebih dahulu, Cek email anda untuk memverifikasi akun!',
+            icon: 'error',
+        }).then(function() {
+    window.location = "index.php";
+});
+    });
+</script>
+<?php endif;?> 
     <!-- Option 1: Bootstrap Bundle with Popper -->
     <script src="../js/bootstrap.bundle.min.js"></script>
 
